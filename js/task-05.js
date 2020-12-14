c108744f91badffee6b32c4e7e47655c3c4adb3f
Klirik <dasoftweb@gmail.com>
2020-12-14 21:35:09 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,22 +1,22 @@
-// const products = [
-//   { name: "Радар", price: 1300, quantity: 4 },
-//   { name: "Сканер", price: 2700, quantity: 3 },
-//   { name: "Дроид", price: 400, quantity: 7 },
-//   { name: "Захват", price: 1200, quantity: 2 },
-// ];
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
 
-// const getAllPropValues = (arr, prop) => {
-//   let result = [];
+const getAllPropValues = (arr, prop) => {
+  const result = [];
 
-//   for (const product of arr) {
-//     if (product[prop]) {
-//       result.push(product[prop]);
-//     }
-//   }
+  for (const product of arr) {
+    if (product[prop]) {
+      result.push(product[prop]);
+    }
+  }
 
-//   return result;
-// };
+  return result;
+};
 
-// console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
-// console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
-// console.log(getAllPropValues(products, "category")); // []
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+console.log(getAllPropValues(products, "category")); // []
